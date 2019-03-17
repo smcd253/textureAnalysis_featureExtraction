181470248794704374c572ff2ff002242d440cae
System Administrator <root@Spencers-MacBook-Air-3.local>
2019-03-17 11:17:12 -0700

    gitHelper.sh

--- siftExample.cpp
+++ siftExample.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
-//#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/xfeatures2d.hpp"`
 #include "opencv2/highgui.hpp"
 
 using namespace cv;
-//using namespace cv::xfeatures2d;
+using namespace cv::xfeatures2d;
 
 void readme();
 
