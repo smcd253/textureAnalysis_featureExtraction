74151b3afd0fe7f51c5b2dbdd9881f2c8c07c968
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2019-03-19 09:33:32 -0700

    gitHelper.sh

--- siftExample.cpp
+++ siftExample.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "opencv2/core.hpp"
 #include "opencv2/features2d.hpp"
-#include "opencv2/xfeatures2d.hpp"`
+#include "opencv2/xfeatures2d.hpp"
 #include "opencv2/highgui.hpp"
 
 using namespace cv;
